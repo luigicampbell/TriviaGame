5a2b20f8287b8c9685102ab6e7d8e978fb603b32
BYRON CAMPBELL <blcampbell@alumni.cpp.edu>
2018-01-07 21:55:37 -0800

    followed tutorial to create classes and constructor

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,12 +1,26 @@
-// var Question(text, answer, choices){
-//   this.text = text;
-//   this.answer = answer;
-//   this.choice = choices;
-// }
-//
-// Question.prototype.correctAnswer = function functionName(choice) {
-//   return choice === this.answer;
-// }
+var Question(text, answer, choices){
+  this.text = text;
+  this.answer = answer;
+  this.choice = choices;
+}
+
+Question.prototype.correctAnswer = function functionName(choice) {
+  return choice === this.answer;
+}
+function Quiz(questions){
+  this.score = 0;
+  this.questions = questions;
+  this.questionIndex = 0;
+}
+Quiz.prototype.getQuestionIndex = function() {
+  return this.questions[this.questionIndex];
+}
+Quiz.prototype.isEnded = function(){
+  return this.questions.length === this.questionIndex;
+}
+Quiz.prototype.guess = function(answer) {
+  
+}
 //
 // var questions = {
 //   diamond:

9ac11899a0a63ae34bde19dab95130e28136810b
BYRON CAMPBELL <blcampbell@alumni.cpp.edu>
2018-01-07 20:33:18 -0800

    added buttons and styling to buttons

---
+++ assets/js/game.js
@@ -0,0 +1,32 @@
+// var Question(text, answer, choices){
+//   this.text = text;
+//   this.answer = answer;
+//   this.choice = choices;
+// }
+//
+// Question.prototype.correctAnswer = function functionName(choice) {
+//   return choice === this.answer;
+// }
+//
+// var questions = {
+//   diamond:
+//   {
+//     name: "Diamond",
+//     value: 0
+//   },
+//   sapphire:
+//   {
+//     name: "Sapphire",
+//     value: 0
+//   },
+//   ruby:
+//   {
+//     name: "Ruby",
+//     value: 0
+//   },
+//   emerald:
+//   {
+//     name: "Emerald",
+//     value: 0
+//   }
+// };
